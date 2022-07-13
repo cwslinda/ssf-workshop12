fa1bec18ea938b7e747c12c46c4d1e91c0056200
Linda Cheung <cws@Lindas-MacBook-Pro.local>
2022-07-13 13:49:03 +0800

    modified generate controller

--- src/main/java/vttp2022/ssf/ssfworkshop12/controller/GenerateController.java
+++ src/main/java/vttp2022/ssf/ssfworkshop12/controller/GenerateController.java
@@ -3,6 +3,9 @@ package vttp2022.ssf.ssfworkshop12.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import vttp2022.ssf.ssfworkshop12.model.Generate;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +20,13 @@ public class GenerateController {
 
     @GetMapping("/")
     public String showGenerateNumForm(Model model){
+        logger.info("----showGenerateNumForm-----");
+        //init an empty generate object that carries an int - x number to be gen
+        Generate genObj = new Generate();
+        //default to 1 everytime the page is loaded
+        genObj.setNumberVal(1);
+        // pass  it to the view as th:obj
+        model.addAttribute("generateObj", genObj);
         return "generatePage";
     }
 }
